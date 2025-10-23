e2233c1c12474cfb44c3d99e8fbdcab01a83420d
zhangkaocheng <zhang819540791@163.com>
2025-10-23 23:23:11 +0800

    部署ERC20时带账户/测试转账及断言

--- test/MyTokenTest.js
+++ test/MyTokenTest.js
@@ -13,8 +13,15 @@ describe("MyToken Test", async()=>{
 
     beforeEach(async()=>{ 
 
+    [account1,account2] = await ethers.getSigners()
+    console.log("账户1:",account1.address)
+    console.log("账户2:",account2.address)
+    console.log("账户1余额:",await ethers.provider.getBalance(account1.address))
+    console.log("账户2余额:",await ethers.provider.getBalance(account2.address))
+
+
     const MyToken = await hre.ethers.getContractFactory("MyToken") //工厂模板
-    MyTokenContract = await MyToken.deploy(initialSupply)
+    MyTokenContract = await MyToken.connect(account2).deploy(initialSupply)  //account2部署
     MyTokenContract.waitForDeployment()
     const contractAddr = await MyTokenContract.getAddress()
     console.log("合约地址:",contractAddr)
@@ -30,6 +37,13 @@ describe("MyToken Test", async()=>{
         console.log("合约地址:",MyTokenContract.getAddress())
         console.log("合约余额:",await ethers.provider.getBalance(MyTokenContract.getAddress()))
         console.log("合约代币余额:",await MyTokenContract.balanceOf(MyTokenContract.getAddress()))
+    })
+
+    it("测试转账", async()=>{ 
+        await MyTokenContract.transfer(account1.address,100) //account2向account1转账100
+        console.log("账户1余额:",await ethers.provider.getBalance(account1.address))
+        console.log("账户2余额:",await ethers.provider.getBalance(account2.address))
+        expect(await MyTokenContract.balanceOf(account1.address)).to.equal(100)
     })
 
 })
